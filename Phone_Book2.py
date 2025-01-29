70d6a2e77cae52c4b65f9f88ff6d8a7271f822b6
5ee5 <marks.sislovs@gmail.com>
2025-01-29 08:54:31 +0200

    Changes

--- Phone_Book2.py
+++ Phone_Book2.py
@@ -1,23 +1,36 @@
 
-contacts = [['Anna', '987654321'], ['Oskars', '123456789'], ['Jenifere', '654987321']]
-contacts_dictionary = {}
+# contacts = [['Anna', '987654321'], ['Oskars', '123456789'], ['Jenifere', '654987321']]
+contacts_dictionary = {
+        "contacts": [
+            {"name": "Anna", "number": "987654321"},
+            {"name": "Oskars", "number": "123456789"},
+            {"name": "Jenny", "number": "654987321"}
+            ]
+        }
+
 while True:
-    response = input("Izvēlies darbību 1-pievienot, 2-izvadīt, 3-izdēst, 0-iziet: ")
+    response = input("Choose: 1-Add, 2-Print, 3-Delete, 0-Exit: ")
 
     if response == '1':
-        print('Pievienosim jaunu kontaktus')
-        name = input("Ievadi vārdu: ")
-        number = input("Ievadi numuru: ")
+        print('Adding new contact')
+        name = input("Enter name: ")
+        number = input("Enter number: ")
         contacts.append([name, number])
     elif response == '2':
-        print('Izvadīsim visus kontaktus')
+        print('Printing all contacts')
         for contact in contacts:
             print(f"{contact[0]} {contact[1]}")
 
     elif response == '3':
+        int(input('Enter the index of the contact you want to delete: '))
         print('Deleting contact')
+        if index < len(contacts):
+            contacts.pop(index)
+        else:
+            print('Index not found')
+
     elif response == '0':
         print('Exiting')
         break
     else:
-        print('Šadas izvēles nav!')
+        print('No such option!')
