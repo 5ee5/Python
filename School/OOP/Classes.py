821fccf3b4d5c8381bd5bbc57cc34feb98fb8d85
5ee5 <marks.sislovs@gmail.com>
2025-02-26 10:27:53 +0200

    finalizing changes

--- School/OOP/Classes.py
+++ School/OOP/Classes.py
@@ -1,12 +1,22 @@
 class Person:
-    def _init_(self, name, age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
+        self.cars = []
 
-person1 = Person()
-person2 = Person()
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
 
-person2.name = "Oscar"
+    def __repr__(self):
+        return f"{self.color} {self.brand}"
 
-print(person1.name)
-print(person2.name)
+person1 = Person("Anna", 17)
+person2 = Person("Oskars", 18)
+
+person1.name = "Jolanta"
+person1.cars = [Car("Volvo", "Black"), Car("Tesla", "White")]
+person2.cars = [Car("Subaru", "Blue")]
+
+print(person1.cars)
