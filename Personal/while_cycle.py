f947660880bf8c5f72fa7428de9f2c04605df7af
5ee5 <marks.sislovs@gmail.com>
2025-03-03 11:38:00 +0200

    clearing up

--- Personal/while_cycle.py
+++
@@ -1,5 +0,0 @@
-n = 1
-
-while n < 10000:
-    print(n)
-    n += 1
--- School/Functions/Name_surname_spliter_list.py
+++
@@ -1,6 +0,0 @@
-def split_text():
-    full_name = input("Enter your full name: ")
-    split_name = full_name.split()
-    return split_name
-
-print(split_text())
