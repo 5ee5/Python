fb6a4d1c4dd5e7d2d3911a98bfae18b188e95b5f
5ee5 <marks.sislovs@gmail.com>
2025-04-02 08:17:27 +0300

    fixing some errors

--- Cookie_monster/main.py
+++ Cookie_monster/main.py
@@ -15,7 +15,9 @@ monster = pygame.image.load('Python/Cookie_monster/assets/monster.png')
 monsterX = 350
 monsterY = 350
 monsterX_change = 0
-monsterX_change = 0
+monsterY_change = 0
+
+cookie = pygame.image.load('Python/Cookie_monster/assets/cookie.png')
 
 clock = pygame.time.Clock()
 
@@ -40,8 +42,8 @@ while running:
                 monsterY_change = 2.5
 
 
-    monsterx += monsterX_change
-    monstery += monsterY_change
+    monsterX += monsterX_change
+    monsterY += monsterY_change
 
     if monsterX < 0:
         monsterX = 0
@@ -52,9 +54,9 @@ while running:
     elif monsterX > 635:
         monsterY = 635
     
-    window.blit(monster, (monsterx, monstery))
+    window.blit(monster, (monsterX, monsterY))
     window.fill('#00ffff')
-    window.blit(monster, (monsterx, monstery))
+    window.blit(monster, (monsterX, monsterY))
 
     pygame.display.update()
     
