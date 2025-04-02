157f88a670f9d75e7e67c345608aaa78a17dd495
5ee5 <marks.sislovs@gmail.com>
2025-04-02 10:27:54 +0300

    Adding cookie function

--- Cookie_monster/main.py
+++ Cookie_monster/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 windowX = 800
@@ -12,6 +13,7 @@ icon = pygame.image.load('Python/Cookie_monster/assets/cookie.png')
 pygame.display.set_icon(icon)
 
 background = pygame.image.load('Python/Cookie_monster/assets/underground.png')
+background = pygame.transform.scale(background, (windowX, windowY))
 monster = pygame.image.load('Python/Cookie_monster/assets/monster.png')
 monsterX = 350
 monsterY = 350
@@ -19,12 +21,27 @@ monsterX_change = 0
 monsterY_change = 0
 
 cookie = pygame.image.load('Python/Cookie_monster/assets/cookie.png')
+cookie = pygame.transform.scale(cookie, (50, 50))
+cookieX = random.randint(0, 636)
+cookieY = random.randint(0, 436)
+cookie_is_eaten = False
+
+while not cookie_is_eaten:
+
+    def cookie_is_eaten(monsterX, monsterY, cookieX, cookieY):
+        distance = ((monsterX - cookieX) ** 2 + (monsterY - cookieY) ** 2) ** 0.5
+        if distance <= 48:
+            return True
+        return False
 
 clock = pygame.time.Clock()
 
+bg_x = (windowX - background.get_width()) // 2
+bg_y = (windowY - background.get_height()) // 2
+
 running = True
 while running:
-    window.blit(background, (0, 0))
+    window.blit(background, (bg_x, bg_y))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -55,9 +72,13 @@ while running:
         monsterX = 735
     elif monsterY > 635:
         monsterY = 635
+
+    if cookie_is_eaten(monsterX, monsterY, cookieX, cookieY):
+        cookieX = random.randint(0, 636)
+        cookieY = random.randint(0, 436)        
     
     window.blit(monster, (monsterX, monsterY))
-
+    window.blit(cookie, (cookieX, cookieY))
     pygame.display.update()
     
     clock.tick(60)
